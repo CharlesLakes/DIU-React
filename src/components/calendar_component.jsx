f68137816b098b71e89499d1e85d8eb6adc80a1a
Carlos Lagos <carlos.lagosc@usm.cl>
2023-11-17 00:37:13 -0300

    calendar

--- src/components/calendar_component.jsx
+++ src/components/calendar_component.jsx
@@ -1,62 +1,34 @@
 import React, { useState } from 'react';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
-import { Paper, Box, Typography } from '@mui/material';
+import { Paper, Box, Typography, TextField, Button} from '@mui/material';
 import EventIcon from '@mui/icons-material/Event';
 
-const CalendarComponent = () => {
+const CalendarComponent = (props) => {
   const [selectedDate, setSelectedDate] = useState(new Date());
   const [dataForDate, setDataForDate] = useState({});
-  
+  const [info,setInfo] = useState(props.info?props.info:[]);
   const handleDateChange = (date) => {
     setSelectedDate(date);
     const isoDate = date.toISOString().split('T')[0];
-    if (isoDate === '2023-10-10') {
-      setDataForDate({
-        date: date,
-        data: 'Reunión Gestion de Ventas',
-      });
-    } else if (isoDate === '2023-10-15') {
-      setDataForDate({
-        date: date,
-        data: 'Reunión Gestion de Organizacion y Equipos',
-      });
-    } else if (isoDate === '2023-10-23') {
-      setDataForDate({
-        date: date,
-        data: 'Reunión SCRUM y Gestion de Proyectos',
-      });
+    let date_found = false;
+    for(const element of info){
+      if(isoDate === element.date){
+        setDataForDate({
+          date: date,
+          data: element.content,
+        });
+        date_found = true;
+        break;
+      }
     }
-    else if (isoDate === '2023-10-29') {
-      setDataForDate({
-        date: date,
-        data: 'Reunión Gestion de Pagos',
-      });
-    }
-    else if (isoDate === '2023-11-21') {
-      setDataForDate({
-        date: date,
-        data: 'Reunión Gestion de Pagos',
-      });
-    }
-    else if (isoDate === '2023-11-23') {
-      setDataForDate({
-        date: date,
-        data: 'Reunión Gestion de Organizacion y Equipos',
-      });
-    }
-    else if (isoDate === '2023-11-24') {
-      setDataForDate({
-        date: date,
-        data: 'Reunión SCRUM y Gestion de Proyectos',
-      });
-    }
-     else {
+    if(!date_found){
       setDataForDate({
         date: date,
         data: 'No hay eventos para este dia',
       });
     }
+
   };
 
   const formatDayAbbreviation = (date) => {
@@ -66,14 +38,28 @@ const CalendarComponent = () => {
   };
 
   const customTileContent = ({ date }) => {
-    const dates = new Set(['2023-10-10', '2023-10-15', '2023-10-23', '2023-10-29', '2023-11-21', '2023-11-23', '2023-11-24'])
+    const dates = [];
+    for(const element of info) dates.push(element.date);
+    const dates_hash = new Set(dates)
     const aux = date.toISOString().split('T')[0]
-    const hasData = dates.has(aux)
+    const hasData = dates_hash.has(aux)
     return (
       <div>
         {hasData && <EventIcon style={{ color: 'red' }} />}
       </div>
     );
+  };
+
+  const addTask = (e) => {
+    e.preventDefault();
+    const isoDate = selectedDate.toISOString().split('T')[0];
+    
+    setInfo(element => [...element,{date: isoDate,content: e.target.desc.value}]);
+    setDataForDate({
+      date: selectedDate,
+      data: e.target.desc.value,
+    });
+    e.target.desc.value = '';
   };
 
   return (
@@ -89,6 +75,17 @@ const CalendarComponent = () => {
           <Paper>
             <Typography variant="h5">{formatDayAbbreviation(selectedDate)} {selectedDate.toLocaleDateString('es-ES', { year: 'numeric', month: 'long', day: 'numeric' })}</Typography>
             <Typography variant="body1">{dataForDate.data}</Typography>
+          </Paper>
+        )}
+      </Box>
+      <Box mt={2}>
+        {selectedDate && (
+          <Paper>
+            <Typography variant="h5" style={{textAlign:"center"}}>Añadir evento</Typography>
+            <form onSubmit={addTask} style={{display: "flex", justifyContent: "center", alignItems: "center",flexDirection: "column",width: "100%"}}>
+              <TextField name="desc" id="outlined-basic" label="Descipción" variant="outlined" style={{width: "90%"}} />
+              <Button type='submit' variant="contained" style={{width: "80%",margin: "5px 0px"}}>Añadir</Button>
+            </form>
           </Paper>
         )}
       </Box>
--- src/pages/calendar.jsx
+++ src/pages/calendar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useState} from 'react'
 import { useNavigate } from "react-router-dom";
 
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
@@ -20,16 +20,47 @@ const NotificationsListItemStyles = {
     textAlign: "center"
 };
 
+let init_info = [
+    {
+        date: '2023-10-10',
+        content: 'Reunión Gestion de Ventas'
+    },
+    {
+        date: '2023-10-15',
+        content: 'Reunión Gestion de Organizacion y Equipos'
+    },
+    {
+        date: '2023-10-23',
+        content: 'Reunión SCRUM y Gestion de Proyectos'
+    },
+    {
+        date: '2023-10-29',
+        content: 'Reunión Gestion de Pagos'
+    },
+    {
+        date: '2023-11-21',
+        content: 'Reunión Gestion de Pagos'
+    },
+    {
+        date: '2023-11-23',
+        content: 'Reunión Gestion de Organizacion y Equipos'
+    },
+    {
+        date: '2023-11-24',
+        content: 'Reunión SCRUM y Gestion de Proyectos'
+    },
+]
+
 const Calendar = () => {
     const navigate = useNavigate();
-    
     return(
         <div className='page'>
             <ArrowBackIcon fontSize='large' style={{cursor:"pointer"}} onClick={() => navigate('/')} />
             <div className='menu'>
                 <h2 style={NotificationsTitleStyles}>Calendario</h2>
-                <CalendarComponent />
+                <CalendarComponent info={init_info} />
             </div>
+
         </div>
     );
 }
